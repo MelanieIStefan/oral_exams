1a16629874c9587a3796ea501d9c47b4ff6b0211
MelanieIStefan <melanie.stefan@cantab.net>
2025-07-19 14:00:55 +0200

    add search keywords, output total number of hits and date of oldest hit

--- data_gathering.py
+++ data_gathering.py
@@ -4,7 +4,7 @@ import requests
 from praw.models import MoreComments
 import pprint
 import pandas as pd
-
+import time
 
 # set credentials and create reddit instance 
 
@@ -23,55 +23,67 @@ reddit = praw.Reddit(client_id=creds['client_id'],
 
 reddit.read_only = True
 
-subreddit="medizinstudium"
 
-# create empty list of data to append to
+def gather_data(subreddit, query):
 
-all_data = []
+    # create empty list of data to append to
+    all_data = []
 
-# extract submissions and comments from a particular subreddit
-for submission in reddit.subreddit(subreddit).hot(limit=1):
-    submission_data = [
-        submission.id,
-        "submission", 
-        submission.author.name, 
-        submission.created_utc, 
-        submission.subreddit.display_name,
-        submission.title,
-        submission.selftext,
-        "n/a",
-        submission.url
-        ]
-    all_data.append(submission_data)
-    
-    # submission.comments.replace_more(limit=None)
-    submission.comments.replace_more(limit=0)
-    for comment in submission.comments.list():
-        comment_data = [
-            comment.id,
-            "comment",
-            comment.author.name,
-            comment.created_utc,
-            comment.subreddit.display_name,
-            "n/a,",
-            comment.body,
-            comment.parent_id,
+    # extract submissions and comments from a particular subreddit
+
+    for submission in reddit.subreddit(subreddit).search(query=query,time_filter="all"):
+        submission_data = [
+            submission.id,
+            "submission", 
+            submission.author, 
+            submission.created_utc, 
+            submission.subreddit.display_name,
+            submission.title,
+            submission.selftext,
             "n/a",
-        ] 
-        all_data.append(comment_data)
+            submission.url
+            ]
+        all_data.append(submission_data)
+        
+        # submission.comments.replace_more(limit=None)
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            comment_data = [
+                comment.id,
+                "comment",
+                comment.author,
+                comment.created_utc,
+                comment.subreddit.display_name,
+                "n/a,",
+                comment.body,
+                comment.parent_id,
+                "n/a",
+            ] 
+            all_data.append(comment_data)
 
 
-# turn list into data frame
+    # turn list into data frame
 
-all_data_frame = pd.DataFrame(all_data)
-all_data_frame.columns = ["ID","Type", "Author", "Created", "Subreddit","Title","Body","Parent ID","url"]
+    all_data_frame = pd.DataFrame(all_data)
+    all_data_frame.columns = ["ID","Type", "Author", "Created", "Subreddit","Title","Body","Parent ID","url"]
 
 
-# save panda as .csv
-filename = subreddit+".csv"
+    # save panda as .csv
+    filename = "./"+subreddit+".csv"
 
-print(filename)
-all_data_frame.to_csv(filename)
+    all_data_frame.to_csv(filename)
+    print(subreddit+": "+str(len(all_data_frame.index)))
+    oldest = min(all_data_frame["Created"])
+    print(time.strftime("%D %H:%M", time.localtime(oldest)))
+
+
+# gather_data(subreddit="medizin")
+
+keywords = '"M1" OR "M3" OR "Physikum" OR "mündlich" OR "mündliche" OR "Prüfung" OR "Staatsexamen" OR "Prüfer" OR "Prüferin"' 
+
+
+# gather_data(subreddit="medizin", query = keywords)
+gather_data(subreddit="medizinstudium", query = keywords)
 
 
 # Docs and Tutorials that helped me do all of this:
