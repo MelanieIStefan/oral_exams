b9271c28299ed281db96780865a3cfae425a5845
MelanieIStefan <melanie.stefan@cantab.net>
2025-07-23 20:41:27 +0200

    finalise data prep

--- data_preparation.py
+++ data_preparation.py
@@ -16,14 +16,37 @@ print(str(len(medizin.index))+" + "+str(len(medizinstudium.index))+" = "+str(len
 print(combined_data.head())
 
 
+
+
+# get rid of NAs in the Body
+combined_data_not_na =  combined_data[(combined_data.Body.notna())]
+
 # filter comments by keyword list
-# df.loc[df['col1'].isin([value1, value2, value3, ...])]
+
+keywords = "M1|M3|Physikum|mündlich|mündliche|Prüfung|Staatsexamen|Prüfer|Prüferin"
 
 
-# save shared data frame
+# mask =   combined_data_not_na["Body"].str.contains("M1")
+mask =   combined_data_not_na["Body"].str.contains(keywords)
+filtered_data = combined_data_not_na[mask]
+
+
+print(filtered_data.head())
+
+
+
+print(str(len(filtered_data.index)))
+
+# save shared and filtered data frame
+
+filename = "./data_combined_prefiltered.csv"
+
+filtered_data.to_csv(filename)
 
 
 
 # Helpful resources:
 # https://pandas.pydata.org/pandas-docs/stable/index.html
 # https://www.statology.org/pandas-select-rows-based-on-column-values/
+# https://stackoverflow.com/questions/50397644/pandas-find-rows-where-a-particular-column-is-not-na-but-all-other-columns-are
+# https://stackoverflow.com/questions/47937697/scalable-solution-for-str-contains-with-list-of-strings-in-pandas
