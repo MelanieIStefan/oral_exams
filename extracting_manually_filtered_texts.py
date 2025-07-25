c0b5f47e19bb95e747151deb14c2334de9c57430
MelanieIStefan <melanie.stefan@cantab.net>
2025-07-25 18:30:08 +0200

    added line to save entire manually selected dataset for some summary stats

--- extracting_manually_filtered_texts.py
+++ extracting_manually_filtered_texts.py
@@ -5,11 +5,14 @@ import pandas as pd
 # import .csv files (change file path as appropriat)
 data_post_manual_filter = pd.read_csv("/home/melanie/Documents/oral_exams/data_combined_manually_filtered.csv")
 
+
 # filter by whether to include the post
 mask =   data_post_manual_filter["Keep?"].str.contains("yes")
 
 
+included_data = data_post_manual_filter[mask]
 included_quotes = data_post_manual_filter[mask].Body
+
 
 
 # check that this worked, and count remaining rows
@@ -20,6 +23,7 @@ print(str(len(included_quotes.index)))
 
 included_quotes.to_csv("data_for_thematic_analysis.txt",sep="\n")
 
+included_data.to_csv("final_dataset.csv")
 
 
 
